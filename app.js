723c59b86f0ef22367a37d9414eece2c3fcd7d25
Veronica Hing <veronica.m.hing@gmail.com>
2020-06-22 18:09:19 -0700

    Added local storage

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 
-let count = -1;//gives each item added a unique id, which is why it's global
+let count = 0;//gives each item added a unique id, which is why it's global
 let adme = document.getElementById('adme');
 let entry = document.querySelector('.entry > input');
-//let todoList = [];
+let clearAll = document.querySelector('.clearAll > button')
 
 class Todo{
-  constructor(){
-    this.entry = entry.value;
+  constructor(entry){
+    this.entry = entry;
     this.id = count;
     this.tile = document.createElement('div');
     this.tile.setAttribute('class', 'newTodo');
@@ -17,20 +17,46 @@ class Todo{
 
   delete(){
     document.body.removeChild(this.tile);
-//    todoList[this.id] = null;
+    localStorage.removeItem(JSON.stringify(this.id));
   }
-}
+}//end Todo Class
 
-//add item to list and push list to local storage
+//add item as string to list and push todo string to local storage
 function addContainer() {
-let todo = new Todo;
-count += 1;
+let todo = new Todo(entry.value);
+localStorage.setItem(JSON.stringify(todo.id), JSON.stringify(todo));
+count += 1;//for next todo item, not current
 let deleteMe = document.getElementById(todo.id);
-deleteMe.addEventListener('click',function(){todo.delete();})
-
-//todoList.push(todo);
-//setLocalStorage(todoList);
-//getList(todoList);
-}
+deleteMe.addEventListener('click',function(){todo.delete();});
+}//end add container
 
 adme.addEventListener('click', function(){addContainer();});
+
+function test(stringy){
+  console.log(stringy);
+}
+//stuff for when page refreshes with tiles remaining on page
+const getLocalStorage = function(){
+  if(localStorage.length < 1){
+    console.log('nothing to regenerate');
+  } else {
+    var entryList = []; //stores all the entries from localStorage
+    for( var i = 0; i < localStorage.length; i++){
+      keyName = localStorage.key(i);
+      tempEntry = JSON.parse(localStorage.getItem(keyName));
+      entryList[i] = tempEntry.entry;
+    }//all entries are now in entryList
+    localStorage.clear(); //clear storage for ids to work
+    for( var i = 0; i < entryList.length; i++){
+      entry.value = entryList[i];
+      addContainer();
+    }//recreate the containers
+  }
+  entry.value = '';
+}
+getLocalStorage();
+
+clearAll.addEventListener('click', function(){
+  console.log('cleared local storage');
+  localStorage.clear();
+});
